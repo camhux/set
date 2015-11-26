20d1ef3ff10a576ae42cfaad9cbade0a65043d98
camhux <camzig@gmail.com>
2015-11-25 17:32:09 -0800

    Comments; clearSet implemented; scaffold for play function

--- set.go
+++ set.go
@@ -63,6 +63,16 @@ func (c *card) String() string {
 // isSet takes in three cards, returning `true` if they
 // compose a set and `false` if not.
 func isSet(a, b, c card) bool {
+	// Because we've modeled attributes as integers 0 <= n <= 2,
+	// we can test validity by taking a sum of each card's values
+	// for a given attribute. There are four valid sums --
+	// three for homogenous attributes:
+	// 0 + 0 + 0 = 0,
+	// 1 + 1 + 1 = 3,
+	// 2 + 2 + 2 = 6.
+	// and one for heterogeneous attributes:
+	// 0 + 1 + 2 = 3.
+	// A modulo by three can check for all of these at once.
 	for i := range a {
 		if (a[i]+b[i]+c[i])%3 != 0 {
 			return false
@@ -71,9 +81,9 @@ func isSet(a, b, c card) bool {
 	return true
 }
 
-// findComplement takes two cards and returns a representation of the
+// complement takes two cards and returns a representation of the
 // card that, if found, would complete a set with the two given cards.
-func findComplement(a, b card) card {
+func complement(a, b card) card {
 	var c card
 	for i := range c {
 		if a[i] == b[i] {
@@ -169,7 +179,12 @@ type setMember struct {
 // as the index in the table slice at which it was found.
 // This enables later removal of these cards from the table.
 func (b *board) findSet() (set [3]setMember, found bool) {
-
+	// First, we create a map of cards to indices by looping through
+	// the table and inserting each card. This helps amortize the work
+	// of checking permutations of sets, since for any pair of cards
+	// we can calculate which third card would complete their set.
+	// With a map of all the cards on the table, we can do this third
+	// check in O(1) time, instead of a third loop of O(n) complexity.
 	mem := make(map[card]int)
 
 	for i, card := range b.table {
@@ -185,10 +200,14 @@ func (b *board) findSet() (set [3]setMember, found bool) {
 outer:
 	for i := range b.table {
 		for j := i; j < len(b.table); j++ {
+			// Here, for each pair of cards as we iterate through the table,
+			// we calculate which third card would create a complete set if present.
+			comp := complement(b.table[i], b.table[j])
 
-			comp := findComplement(b.table[i], b.table[j])
-
+			// Next, we check to see if that third card is on the table.
 			if compIx, ok := mem[comp]; ok {
+				// If it is, we mark our `found` return value as true, and we instantiate
+				// and assign a results array containing the setMembers of the set we've found.
 				found = true
 				set = [3]setMember{
 					{
@@ -204,7 +223,8 @@ outer:
 						compIx,
 					},
 				}
-
+				// Here, we break the outer loop, since a set has been found and there's
+				// no point in continuing to look.
 				break outer
 			}
 		}
@@ -215,7 +235,28 @@ outer:
 	return
 }
 
-// startGame creates and shuffles a new deck, associates this deck with
+// clearSet takes a set of cards as input and replaces the board's
+// table with a slice omitting the cards of the set.
+func (b *board) clearSet(set [3]setMember) {
+	// Here we create the slice that will become the new table.
+	// We want to shift cards around to new indices to fill the gaps,
+	// which `append` makes convenient, but we also don't want to
+	// repeatedly allocate a new underlying array when it's not
+	// necessary, so we create the slice with length 0 but adequate capacity.
+	t := make([]card, 0, len(b.table)-3)
+
+outer:
+	for _, card := range b.table {
+		for _, setMember := range set {
+			if setMember == card {
+				continue outer
+			}
+		}
+		t = append(t, card)
+	}
+}
+
+// newGame creates and shuffles a new deck, associates this deck with
 // a new board, deals out twelve cards to the board's table, then returns
 // the address of the board.
 func newGame() *board {
@@ -223,4 +264,13 @@ func newGame() *board {
 	b := &board{deck: deck}
 	b.dealTwelve()
 	return b
+}
+
+// play executes one "tick" of the game -- it attempts to find a set, and if it
+// succeeds, it removes that set and appends it to a log of sets found, then deals
+// out more cards if necessary. If no set is found, play will deal out three more
+// cards, then return and cede control. play returns true if the game is finished
+// (i.e., no more sets and no more cards to deal) and false if the game can continue.
+func (b *board) play(log []string) (done bool) {
+
 }
